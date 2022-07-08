735fc48ced8212c77da582eda4020dce9e478ca6
Harshavardhan912 <99498633+Harshavardhan912@users.noreply.github.com>
2022-07-08 11:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib 
 
 #load the joblib model 
-model_nb = joblib.load('spam.csv')
+model_nb = joblib.load('spam-ham')
 
 #user input 
 st.title("Email message a Spam Or Ham")
